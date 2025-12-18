6013ee10c085b914ff80639f94a08e5231aa68ec
Samuel Fjellet <229708690+Samuel-Fjellet@users.noreply.github.com>
2025-12-18 10:17:28 +0100

    App Try changes

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,22 +10,22 @@ import java.time.LocalDateTime;
 public class App {
     static void main(String[] args) {
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("restaurantPU");
-        EntityManager em = emf.createEntityManager();
+        try (EntityManagerFactory emf = Persistence.createEntityManagerFactory("restaurantPU");
+             EntityManager em = emf.createEntityManager()) {
 
-        try{
-            em.getTransaction().begin();
-            //BookingTable bookingTable = new BookingTable(6, 8);
-            Booking booking = new Booking(2L ,LocalDateTime.now(), 6);
-            //Guest guest = new Guest();
-            em.persist(booking);
-            em.getTransaction().commit();
-        } catch (Exception e){
-            em.getTransaction().rollback();
+            try {
+                em.getTransaction().begin();
+                //BookingTable bookingTable = new BookingTable(6, 8);
+                Booking booking = new Booking(2L, LocalDateTime.now(), 6);
+                //Guest guest = new Guest();
+                em.persist(booking);
+                em.getTransaction().commit();
+            } catch (Exception e) {
+                em.getTransaction().rollback();
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            em.close();
-            emf.close();
         }
     }
 }
